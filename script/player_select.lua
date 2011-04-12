a384316608209c66ee86b57740944cd2b6b4f18b
nagoya313 <nagoya313@gmail.com>
2011-04-12 21:25:13 +0900

    それなりに構造化

--- script/player_select.lua
+++ script/player_select.lua
@@ -1,88 +1,123 @@
-p_table = {'reimu_up', 'reimu_down', 'marisa_up', 'marisa_down'}
-local image_table = {'image/slpl00a.dds', 'image/slpl00b.dds', 'image/slpl01a.dds', 'image/slpl01b.dds'}
-local p_select = 1
+kPlayerList = {
+  {key = 'reimu_up', file = 'image/slpl00a.dds'},
+  {key = 'reimu_down', file = 'image/slpl00b.dds'},
+  {key = 'marisa_up', file = 'image/slpl01a.dds'},
+  {key = 'marisa_down', file = 'image/slpl01b.dds'}
+}
+local kPlayerSelectImageFileName = 'image/select03.dds'
+local kPlayerMin = 1
+local kPlayerMax = 3
 
-local function init(mode_select)
-  move_actor(mode_table[mode_select], 32, 192)
-  set_actor_alpha(mode_table[mode_select], 0x80)
-  for i, key in ipairs(p_table) do
-    add_bright_image(key, image_table[i])
-    resize_actor(key, 128, 128)
-    set_actor_uv(key, 0, 0, 128, 128)
+local function init(mode_select, character_select)
+  move_actor(kModeList[mode_select], 32, 192)
+  set_actor_alpha(kModeList[mode_select], 0x80)
+  for i, player in ipairs(kPlayerList) do
+    add_bright_image(player.key, player.file)
+    resize_actor(player.key, 128, 128)
+    set_actor_uv(player.key, 0, 0, 128, 128)
     if i % 2 == 0 then
-      move_actor(key, 256, 160)
+      move_actor(player.key, 256, 160)
     else
-      move_actor(key, 256, 32)
+      move_actor(player.key, 256, 32)
     end
   end
-  if p_select == 1 then
-    sleep_actor(p_table[3])
-    sleep_actor(p_table[4])
+  if character_select == 1 then
+    sleep_actor(kPlayerList[3].key)
+    sleep_actor(kPlayerList[4].key)
   else
-    sleep_actor(p_table[1])
-    sleep_actor(p_table[2])
+    sleep_actor(kPlayerList[1].key)
+    sleep_actor(kPlayerList[2].key)
   end
-  add_image('player_select', 'image/select03.dds')
+  add_image('player_select', kPlayerSelectImageFileName)
   resize_actor('player_select', 128, 32)
   move_actor('player_select', 172, 24)
   set_actor_uv('player_select', 0, 0, 128, 32)
 end
 
 local function clean()
-  for i, key in ipairs(p_table) do
-    erase_actor(key)
+  for i, player in ipairs(kPlayerList) do
+    erase_actor(player.key)
   end
   erase_actor('player_select')
 end
 
-local function clean2()
+local function reset(character_select)
+  move_actor(kPlayerList[character_select].key, 256, 32)
+  move_actor(kPlayerList[character_select + 1].key, 256, 160)
+  set_actor_alpha(kPlayerList[character_select].key, 0xFF)
+  set_actor_alpha(kPlayerList[character_select + 1].key, 0xFF)
+  add_image('player_select', kPlayerSelectImageFileName)
+  resize_actor('player_select', 128, 32)
+  move_actor('player_select', 172, 24)
+  set_actor_uv('player_select', 0, 0, 128, 32)
+end
+
+local function clean_to_spell_select()
   erase_actor('player_select')
 end
 
-local function update(mode_select)
+local function change_not_select(character_select)
+  sleep_actor(kPlayerList[character_select].key)
+  sleep_actor(kPlayerList[character_select + 1].key)
+end
+
+local function change_select(character_select)
+  active_actor(kPlayerList[character_select].key)
+  active_actor(kPlayerList[character_select + 1].key)
+end
+
+local function select_right(character_select)
+  stop_se('select')
+  play_se('select', 1)
+  change_not_select(character_select)
+  character_select = character_select + 2 > kPlayerMax and kPlayerMin or kPlayerMax 
+  change_select(character_select)
+  return character_select
+end
+
+local function select_left(character_select)
+  stop_se('select')
+  play_se('select', 1)
+  change_not_select(character_select)
+  character_select = character_select - 2 < kPlayerMin and kPlayerMax or kPlayerMin 
+  change_select(character_select)
+  return character_select
+end
+
+local function decide(character_select)
+  stop_se('ok')
+  play_se('ok', 1)
+  coroutine.yield()
+  clean_to_spell_select()
+  spell_select(character_select)
+  reset(character_select)
+end
+
+local function cancel()
+  stop_se('cancel')
+  play_se('cancel', 1)
+  coroutine.yield()
+  clean()
+end
+
+local function update(mode_select, character_select)
   while true do
     if key_triger(kRight) == true then
-      stop_se('select')
-      play_se('select', 1)
-      sleep_actor(p_table[p_select])
-      sleep_actor(p_table[p_select + 1])
-      p_select = p_select + 2 > 3 and 1 or 3 
-      active_actor(p_table[p_select])
-      active_actor(p_table[p_select + 1])
+      character_select = select_right(character_select)
     elseif key_triger(kLeft) == true then
-      stop_se('select')
-      play_se('select', 1)
-      sleep_actor(p_table[p_select])
-      sleep_actor(p_table[p_select + 1])
-      p_select = p_select - 2 < 1 and 3 or 1 
-      active_actor(p_table[p_select])
-      active_actor(p_table[p_select + 1])
+      character_select = select_left(character_select)
     elseif key_triger(kCircle) == true then
-      stop_se('ok')
-      play_se('ok', 1)
-      coroutine.yield()
-      clean2()
-      spell_select(p_select, mode_select)
+      decide(character_select)
     elseif key_triger(kCross) == true then
-      stop_se('cancel')
-      play_se('cancel', 1)
-      coroutine.yield()
-      clean()
-      mode2(mode_select)
+      cancel()
+      break
     end
     coroutine.yield()
   end
 end
 
 function player_select(mode_select)
-  init(mode_select)
-  update(mode_select)
-end
-
-function player_select2(mode_select)
-  add_image('player_select', 'image/select03.dds')
-  resize_actor('player_select', 128, 32)
-  move_actor('player_select', 172, 24)
-  set_actor_uv('player_select', 0, 0, 128, 32)
-  update(mode_select)
+  local character_select = 1
+  init(mode_select, character_select)
+  update(mode_select, character_select)
 end
