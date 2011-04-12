b7ad8b6761328e96e88cb0435d3df2ae14ee63f5
nagoya313 <nagoya313@gmail.com>
2011-04-12 17:16:56 +0900

    bgmテーブルを修正

--- script/music_table.lua
+++ script/music_table.lua
@@ -1,23 +1,19 @@
-kMusicFileNameList = {
-  'bgm/th06_01.wav'
-}
-
-kMusicIntroOffsetList = {
-  0x102670,
-  0x37E5D4,
-  0x07BCA4,
-  0x128C9C,
-  0x0F9640,
-  0x7919C8,
-  0x3F5B30,
-  0x0B2818,
-  0x4F19FC,
-  0x373E7C,
-  0x0E9D18,
-  0x0A2180,
-  0x595D74,
-  0x608258,
-  0x0C07F0,
-  0x186D40,
-  0x0E9E20
+kMusicList = {
+  {offset = 0x102670, file = 'bgm/th06_01.wav'},
+  {offset = 0x37E5D4, file = 'bgm/th06_02.wav'},
+  {offset = 0x07BCA4, file = 'bgm/th06_03.wav'},
+  {offset = 0x128C9C, file = 'bgm/th06_04.wav'},
+  {offset = 0x0F9640, file = 'bgm/th06_05.wav'},
+  {offset = 0x7919C8, file = 'bgm/th06_06.wav'},
+  {offset = 0x3F5B30, file = 'bgm/th06_07.wav'},
+  {offset = 0x0B2818, file = 'bgm/th06_08.wav'},
+  {offset = 0x4F19FC, file = 'bgm/th06_09.wav'},
+  {offset = 0x373E7C, file = 'bgm/th06_10.wav'},
+  {offset = 0x0E9D18, file = 'bgm/th06_11.wav'},
+  {offset = 0x0A2180, file = 'bgm/th06_12.wav'},
+  {offset = 0x595D74, file = 'bgm/th06_13.wav'},
+  {offset = 0x608258, file = 'bgm/th06_14.wav'},
+  {offset = 0x0C07F0, file = 'bgm/th06_15.wav'},
+  {offset = 0x186D40, file = 'bgm/th06_16.wav'},
+  {offset = 0x0E9E20, file = 'bgm/th06_17.wav'}
 }
